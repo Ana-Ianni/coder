8c087d110adc6ebf697b4067c7eccadbcc82e2ba
Ana Ianni <70654287+Ana-Ianni@users.noreply.github.com>
2022-02-14 23:34:15 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,93 @@
+import { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, Button, View, FlatList, Modal } from 'react-native';
+import AddItem from './components/AddItem/AddItem';
+
+export default function App() {
+  const [textInput, setTextInput] = useState('')
+  const [itemList, setItemList] = useState([])
+
+  const [itemSelected, setItemSelected] = useState({})
+  const [modalVisible, setModalVisible] = useState(false)
+
+  const handleChangeText = (text) => {
+    setTextInput(text)
+  }
+
+  const handleOnPress = () => {
+    setTextInput('')
+    setItemList([
+      ...itemList,
+      {
+        value: textInput,
+        id: Math.random().toString(),
+      },
+    ])
+  }
+
+  const handleOnDelete = (item) => () => {
+    setModalVisible(true)
+    setItemSelected(item)
+  }
+
+  const handleConfirmDelete = () => {
+    const { id } = itemSelected
+    setItemList(itemList.filter(item => item.id !== id))
+    setModalVisible(false)
+    setItemSelected({})
+  }
+
+  return (
+    <View style={styles.container}>
+      <AddItem
+        textInput={textInput}
+        handleOnPress={handleOnPress}
+        handleChangeText={handleChangeText}
+      />
+
+      <FlatList
+        data={itemList}
+        renderItem={({ item }) => (
+            <View style={styles.item}>
+              <Text>{item.value}</Text>
+              <Button onPress={handleOnDelete(item)} title="X" />
+            </View>
+          )
+        }
+        keyExtractor={item => item.id}
+      />
+
+      <Modal animationType='slide' visible={modalVisible}>
+        <View>
+          <View>
+            <Text>¿Está seguro que desea eliminar?</Text>
+            <Text>{itemSelected.value}</Text>
+          </View>
+          <View>
+            <Button
+              onPress={handleConfirmDelete}
+              title="CONFIRMAR"
+            />
+          </View>
+        </View>
+      </Modal>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 30,
+  },
+  item: {
+    padding: 20,
+    marginVertical: 20,
+    borderColor: 'black',
+    borderWidth: 1, 
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  }
+});
---
+++ README.md
@@ -0,0 +1,27 @@
+# app-shopping-list
+
+## Coderhouse - Desarrollo de Aplicaciones
+
+Clase 3 (Core Components)
+
+Clase 4 (Eventos & Lista Optimizada)
+
+Clase 5 (Debug & Split Components)
+
+## Versiones
+
+Main siempre es la última versión
+
+Ubica los ejemplos de cada clase en la rama correspondiente
+
+## Usar este repo
+
+Clona el repo en tu máquina
+
+En la terminal, cambia a la carpeta creada e instala los módulos. Cuando termine, inicia el proyecto con Expo:
+
+```
+$ cd app-todo
+$ npm install
+$ expo start
+```
